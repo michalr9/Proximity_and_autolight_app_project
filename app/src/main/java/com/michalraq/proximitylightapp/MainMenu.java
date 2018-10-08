5d115ba2bf6f6b15587f9756b5dbcdc193aa9f92
michalraq <raku9611@gmail.com>
2018-10-08 15:01:55 +0200

    Przenoszenie do nowej aktywnosci po kliknieciu przycisku

---
+++ app/src/main/java/com/michalraq/proximitylightapp/MainMenu.java
@@ -0,0 +1,27 @@
+package com.michalraq.proximitylightapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+    }
+
+    public void onButtonLightClick(View v){
+        Toast myToast = Toast.makeText(getApplicationContext(),"Zarządzaj światłem !",Toast.LENGTH_LONG);
+
+        startActivity(new Intent(this,Places.class));
+
+        myToast.show();
+
+    }
+}
