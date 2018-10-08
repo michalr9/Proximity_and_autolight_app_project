dc6609a10ca66d447612672bd59cb8869df376a5
michalraq <raku9611@gmail.com>
2018-10-08 18:40:04 +0200

    Autostart BT dziala

--- app/src/main/java/com/michalraq/proximitylightapp/MainMenu.java
+++ app/src/main/java/com/michalraq/proximitylightapp/MainMenu.java
@@ -1,18 +1,38 @@
 package com.michalraq.proximitylightapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
+    BluetoothAdapter mBtAdapter;
+    Boolean isBTActive;
+
+
+    @Override
+    protected void onStart(){
+        enableBT();
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        /*BLUETOOTH*/
+        isBTActive = false;
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        /*WIFI*/
+
 
     }
 
@@ -23,4 +43,37 @@ public class MainMenu extends AppCompatActivity {
         startActivity(new Intent(this,Places.class));
 
     }
+
+
+    public void enableBT(){
+         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBtAdapter == null) {
+            Toast.makeText(this, "Twoje urządzenie nie umożliwia korzystania z BT", Toast.LENGTH_SHORT).show();
+            isBTActive=false;
+        }
+
+        if (! mBtAdapter.isEnabled()) {
+              mBtAdapter.enable();
+
+            isBTActive=true;
+            Toast.makeText(this, "Aktywowano Bluetooth !", Toast.LENGTH_SHORT).show();
+        }
+
+        if(mBtAdapter.isEnabled()){
+            isBTActive = true;
+        }
+    }
+
+    public void enableWiFi(){
+
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        Log.d("MainMenu","Wywolano destroy");
+        super.onDestroy();
+    }
+
 }
